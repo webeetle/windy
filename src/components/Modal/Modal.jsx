2870fe47f03c015426a9bd6453556ae570f565a2
aldo noschese <aldo.noschese@webeetle.it>
2021-06-24 15:26:09 +0200

    Start modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,43 +2,20 @@ import React from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 
-import Button from "../Button/Button";
-
 const Modal = ({
-  //   isOpen = false,
+  isOpen = false,
   title = "Dialog",
-  actions = [
-    <Button layout="bordered" color="blue">
-      Ciao
-    </Button>,
-  ],
+  actions = [],
+  onClose = () => null,
   children,
 }) => {
-  let [opened, setOpened] = useState(false);
-
-  function closeModal() {
-    setOpened(false);
-  }
-
-  function openModal() {
-    setOpened(true);
-  }
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          Open dialog
-        </button>
-      </div>
-      <Transition appear show={opened} as={Fragment}>
+      <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
           className="fixed inset-0 z-10 overflow-y-auto"
-          onClose={closeModal}
+          onClose={() => (typeof onClose() === "function" ? onClose() : null)}
         >
           <div className="min-h-screen px-4 text-center">
             <Transition.Child
@@ -78,11 +55,7 @@ const Modal = ({
                 <div className="flex items-center space-x-5 mt-4">
                   {actions &&
                     actions.map((Button) => {
-                      return (
-                        <div>
-                          <Button />
-                        </div>
-                      );
+                      return <div>{Button}</div>;
                     })}
                 </div>
               </div>
@@ -95,13 +68,3 @@ const Modal = ({
 };
 
 export default Modal;
-
-{
-  /* <button
-type="button"
-className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-onClick={closeModal}
->
-Got it, thanks!
-</button> */
-}
--- src/components/Modal/Modal.vitro.jsx
+++ src/components/Modal/Modal.vitro.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import "tailwindcss/dist/tailwind.css";
+import Button from "../Button/Button";
 
 export const Contained = () => {
+  const [opened, setOpened] = useState(false);
+
+  function closeModal() {
+    setOpened(false);
+  }
+
+  function openModal() {
+    setOpened(true);
+  }
+
   return (
     <div className="flex items-center space-x-5">
-      <Modal>Sono Una Modale</Modal>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
+      <Modal
+        isOpen={opened}
+        actions={[
+          <Button
+            onClick={closeModal}
+            layout="bordered"
+            color="blue"
+            rounded="md"
+          >
+            Ciao
+          </Button>,
+        ]}
+      >
+        I'm the modal
+      </Modal>
     </div>
   );
 };
