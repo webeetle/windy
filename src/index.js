94a31e3391e88b048855698ae95e1a7f84d5a900
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2021-06-26 11:19:17 +0200

    added Badge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import Button from "./components/Button/Button.jsx";
 import Panel from "./components/Panel/Panel.jsx";
 import Dropdown from "./components/Dropdown/Dropdown.jsx";
+
 import Input from "./components/Form/Input/Input.jsx";
 import Checkbox from "./components/Form/Checkbox/Checkbox.jsx";
+import Radio from "./components/Form/Radio/Radio.jsx";
+import Select from "./components/Form/Select/Select.jsx";
+
 import Modal from "./components/Modal/Modal.jsx";
-export { Button, Panel, Dropdown, Input, Checkbox, Modal };
+import Badge from "./components/Badge/Badge.jsx";
+
+export {
+  Button,
+  Panel,
+  Dropdown,
+  Input,
+  Checkbox,
+  Radio,
+  Select,
+  Modal,
+  Badge,
+};
